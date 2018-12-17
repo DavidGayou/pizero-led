7e007e0b207834b107e5f9bc980280579ab5d936
David Gayou <david.gayou@openvalue.fr>
2018-12-17 21:43:08 +0100

    First iteration

---
+++ src/python/ledcontroller.py
@@ -0,0 +1,22 @@
+
+
+from flask import Flask
+from neopixel import *
+
+
+# LED strip configuration:
+LED_COUNT = 30      # Number of LED pixels.
+LED_PIN = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10       # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
+
+
+app = Flask(__name__)
+
+app.route('/')
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run()
