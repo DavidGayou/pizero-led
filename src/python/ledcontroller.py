903aca331e176012891b6eff3eefaa34c170b749
David Gayou <david.gayou@openvalue.fr>
2018-12-17 22:43:21 +0100

    Add some operations

--- src/python/ledcontroller.py
+++ src/python/ledcontroller.py
@@ -90,8 +90,22 @@ def hello():
 @app.route('/rainbow')
 def runRainbow():
     rainbow(strip)
+    return "Rainbow"
 
+
+@app.route('/ColorWipeRed')
+def runRainbow():
+    colorWipe(strip,Color(255, 0, 0))
+    return "ColorWipeRed"
+
+@app.route('/ColorWipeBlue')
+def runRainbow():
+    colorWipe(strip,Color(0, 255, 0))
+    return "ColorWipeBlue"
 
 
 if __name__ == '__main__':
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Intialize the library (must be called once before other functions).
+    strip.begin()
     app.run(host='0.0.0.0')
