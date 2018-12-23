faecdb42493d89329c596976bf92b42ee5e25752
David Gayou <david.gayou@openvalue.fr>
2018-12-23 12:33:20 +0100

    Split the pull

--- src/sh/boot.sh
+++ src/sh/boot.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 cd /home/pi/dev/pizero-led/
-echo "Git pull from pizero-led"
-/usr/bin/git pull
+echo "Git fetch from pizero-led"
+/usr/bin/git fetch
+echo "git reset --hard on origin"
+/usr/bin/git reset --hard origin/master
 exit 0
