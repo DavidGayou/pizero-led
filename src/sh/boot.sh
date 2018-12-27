44892a840832532a0c9607d07743b687fd23400f
tarnefys <tarnefys@gmail.com>
2018-12-27 15:01:24 +0100

    Fix the wait time in the boot.sh

--- src/sh/boot.sh
+++ src/sh/boot.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
 cd /home/pi/dev/pizero-led/
-curl trnfs.com/thisispizero
 sleep 10
-echo "Git fetch from pizero-led"
+echo "Launched boot.sh" >> /home/pi/dev/pizero-led/boot.log
+curl trnfs.com/thisispizero
+
+echo "Git fetch from pizero-led" >> /home/pi/dev/pizero-led/boot.log
 /usr/bin/git fetch
-echo "git reset --hard on origin"
+echo "git reset --hard on origin" >> /home/pi/dev/pizero-led/boot.log
 /usr/bin/git reset --hard origin/master
 
 nohup sudo PYTHONPATH=".:build/lib.linux-armv7l-2.7"  python /home/pi/dev/pizero-led/src/python/ledcontroller.py  >/home/pi/dev/pizero-led/run.log 2>&1
