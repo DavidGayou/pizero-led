96ae07c690945b4fbd6825fdd11684e68a2586ac
tarnefys <tarnefys@gmail.com>
2018-12-29 15:57:38 +0100

    Switch off last pixel of shootingStar

--- src/python/ledcontroller.py
+++ src/python/ledcontroller.py
@@ -153,6 +153,8 @@ def shootingStarAtPos(strip, pos, queue_length):
         if (pos-i) > 0:
             queuePosColor=155-(10*i)
             strip.setPixel(pos-i, Color(queuePosColor, queuePosColor, queuePosColor) )
+    if (pos - queue_length) >0:
+        strip.setPixel(pos-queue_length, Color(0, 0, 0) )
 
     
 
